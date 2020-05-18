a266c622a951ee5cb1ded4f47c9b6c30ca6754ee
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-05-18 00:55:42 -0300

    Separando servidor da aplicação

--- bin/server.js
+++ bin/server.js
@@ -1,24 +1,11 @@
-'use strict'
-
+const app = require('../src/app');
+const debug = require('debug')('panzer:server');
 const http = require('http');
-const debug = require('debug')('nodestr:server');
-const express = require('express');
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.1"
-  });
-});
-
-app.use("/", route);
 
 server.listen(port);
 server.on('error', onError);
