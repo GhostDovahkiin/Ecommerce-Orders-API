ca41e4e742807cc6f52802b0921ac936f85766b5
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-05-18 19:58:41 -0300

    CRUD criado

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 const route = router.get('/', (req, res, next) => {
   res.status(200).send({
@@ -12,6 +18,24 @@ const route = router.get('/', (req, res, next) => {
   });
 });
 
+const create = router.post('/', (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  res.status(20).send({
+    id: id,
+    item: req.body
+  });
+});
+
+const del = router.delete('/', (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
 app.use("/", route);
+app.use("/products", create);
+app.use("products", put);
 
 module.exports = app;
