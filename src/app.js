2f4f8a55fc8b1eaa767817a32dd84074bbc258dd
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-05-18 23:46:27 -0300

    Rotas definidas e separadas

--- src/app.js
+++ src/app.js
@@ -6,36 +6,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+// Carrega rotas
+const indexRoute = require('./routes/indexRoute');
+const productRoute = require('./routes/productRoute');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-const route = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.1"
-  });
-});
-
-const create = router.post('/', (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-  const id = req.params.id;
-  res.status(20).send({
-    id: id,
-    item: req.body
-  });
-});
-
-const del = router.delete('/', (req, res, next) => {
-  res.status(200).send(req.body);
-});
-
-app.use("/", route);
-app.use("/products", create);
-app.use("products", put);
+app.use("/", indexRoute);
+app.use("/products", productRoute);
 
 module.exports = app;
