1e68b6944cfebe60742b5c985cbf7db25bc341ac
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-05-19 00:03:59 -0300

    CRUD terminado

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,21 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/productController');
 
-router.post('/', (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-router.put('/:id', (req, res, next) => {
-  const id = req.params.id;
-  res.status(20).send({
-    id: id,
-    item: req.body
-  });
-});
-
-router.delete('/', (req, res, next) => {
-  res.status(200).send(req.body);
-});
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;
