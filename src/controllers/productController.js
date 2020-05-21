a94f570a68000791f48003bbcc135fbfde63d249
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-05-21 03:21:45 -0300

    Validation

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validators');
 
 exports.get = (req, res, next) => {
   Product
@@ -52,6 +53,16 @@ exports.getByTag = (req, res, next) => {
 }
 
 exports.post = (req, res, next) => {
+  let contract = new ValidationContract();
+  contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
+  contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres');
+  contract.hasMinLen(req.body.description, 10, 'A descrição deve conter pelo menos 10 caracteres');
+
+  if (contract.isValid()) {
+    res.status(400).send(contract.errors()).end();
+    return;
+  }
+
   var product = new Product(req.body);
   product
     .save()
