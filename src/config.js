1ec88db3af7e7f9614f79cdaee253b2e9379ebf8
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-02 23:44:12 -0300

    Arquivo de configuração pronto

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+  connectionString: "mongodb+srv://hearthfire:thesims2@clusterpicudo-s9otl.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  sendGridKey: 'TBD',
+  containerConnectionString: 'TBD'
+}
