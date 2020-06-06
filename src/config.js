f92f70c591f69f0803e76c69cae01355733df7f1
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-06 01:06:14 -0300

    Protecting keys

--- src/config.js
+++ src/config.js
@@ -2,7 +2,5 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à DRACO Imports! \nSeu cadastro foi finalizado e você agora tem acesso ao nosso Ecommerce! \n\nAproveite!';
 
 module.exports = {
-  connectionString: "mongodb+srv://hearthfire:thesims2@clusterpicudo-s9otl.gcp.mongodb.net/test?retryWrites=true&w=majority",
-  sendGridKey: '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',
   containerConnectionString: 'TBD'
 }
