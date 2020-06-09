507e08eb206844cc68a8503051247c49eca03954
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-08 23:56:56 -0300

    Habilitando o CORS

--- src/app.js
+++ src/app.js
@@ -33,6 +33,14 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+// Habilita o CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use("/", indexRoute);
 app.use("/orders", orderRoute);
 app.use("/products", productRoute);
