70ba8e8a62503d375688674fd288f29fb29c30f5
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-08 23:19:59 -0300

    refresh token

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -76,3 +76,36 @@ exports.authenticate = async (req, res, next) => {
     });
   }
 };
+
+exports.refreshToken = async (req, res, next) => {
+  try {
+    var token = req.body.token || req.query.token || req.headers['x-acess-token'];
+    const data = await authService.decodeToken(token);
+
+    const customer = await repository.getById(data._id);
+
+    if (!customer) {
+      res.status(404).send({
+        message: 'Cliente não encontrado'
+      });
+      return;
+    }
+    const tokenData = await authService.generateToken({
+      id: customer._id,
+      email: customer.email,
+      name: customer.name
+    });
+
+    res.status(201).send({
+      token: tokenData,
+      data: {
+        email: customer.email,
+        name: customer.name
+      }
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao processar sua requisição'
+    });
+  }
+};
--- src/repository/customer-repository.js
+++ src/repository/customer-repository.js
@@ -14,3 +14,8 @@ exports.authenticate = async (data) => {
   });
   return res;
 }
+
+exports.getById = async (id) => {
+  const res = await Customer.findById(id);
+  return res;
+}
