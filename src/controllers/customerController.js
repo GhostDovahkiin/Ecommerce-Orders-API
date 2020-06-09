c74f3393d9115149686fcb94999a4e0b75618604
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-06-08 22:55:36 -0300

    recuperando dados de usuário logado

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -58,6 +58,7 @@ exports.authenticate = async (req, res, next) => {
       return;
     }
     const token = await authService.generateToken({
+      id: customer._id,
       email: customer.email,
       name: customer.name
     });
